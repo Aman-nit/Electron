c31aeaebb7d422174122763e167a7cbb827e45c2
Md Aman <143195620+Aman-nit@users.noreply.github.com>
2024-09-11 16:57:49 +0530

    Add files via upload

---
+++ Social Media Investigation/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const onlineStatusWindow = new BrowserWindow({
+    width: 900,
+    height: 700
+  })
+
+  onlineStatusWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
